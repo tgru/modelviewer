901e89b3c71e53c42e9cbb4c404d5cc7a4c3902d
tgru <21686590+tgru@users.noreply.github.com>
2019-03-05 21:00:17 +0100

    Add ModelRenderer interface.

---
+++ js/ModelRenderer.js
@@ -0,0 +1,18 @@
+class ModelRenderer {
+    // build scene and canvas with given size
+    constructor(width, height) {}
+
+    // load model
+    load(path) {}
+
+    // release resources
+    destroy() {}
+
+    // return canvas
+    getCanvas() {}
+
+    // resize canvas
+    resize(width, height) {
+
+    }
+}
