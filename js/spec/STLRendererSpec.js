fb920b3e9806007a77628c9dcb66ad4f3ebe1bda
tgru <21686590+tgru@users.noreply.github.com>
2019-03-06 17:11:25 +0100

    Add unit tests for constructor of STLRenderer

---
+++ js/spec/STLRendererSpec.js
@@ -0,0 +1,35 @@
+var STLRenderer = require('../STLRenderer')
+
+describe("STLRenderer", function () {
+    describe("constructor(width, height)", function () {
+        it("Should build without error", function () {
+            expect(function () {
+                renderer = new STLRenderer(800, 600)
+            }).not.toThrowError()
+        })
+
+        it("Should not accept a negative width", function () {
+            expect(function () {
+                renderer = new STLRenderer(-800, 600)
+            }).toThrowError()
+        })
+
+        it("Should not accept a negative height", function() {
+            expect(function () {
+                renderer = new STLRenderer(800, -600)
+            }).toThrowError()
+        })
+
+        it("Should not accept zero width", function() {
+            expect(function () {
+                renderer = new STLRenderer(0, 600)
+            }).toThrowError()
+        })
+
+        it("Should not accept zero height", function() {
+            expect(function () {
+                renderer = new STLRenderer(800, 0)
+            }).toThrowError()
+        })
+    })
+})
