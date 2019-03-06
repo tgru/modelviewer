3f772d7f0654dcb4e8a9239f445afe343ca3352e
tgru <21686590+tgru@users.noreply.github.com>
2019-03-06 17:09:11 +0100

    Add empty class STLRenderer extending ModelRenderer

---
+++ js/STLRenderer.js
@@ -0,0 +1,23 @@
+var ModelRenderer = require('./ModelRenderer')
+
+module.exports = class STLRenderer extends ModelRenderer {
+    constructor(width, height) {
+        super(width, height)
+    }
+
+    load(path) {
+
+    }
+
+    destroy() {
+
+    }
+
+    getCanvas() {
+
+    }
+
+    resize(width, height) {
+
+    }
+}
