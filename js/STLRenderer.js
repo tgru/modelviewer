a8e01838d7bf443f6be693426254bbd5bb1581d2
tgru <21686590+tgru@users.noreply.github.com>
2019-04-19 18:44:25 +0200

    Change x/y rotation to only z rotation

--- js/STLRenderer.js
+++ js/STLRenderer.js
@@ -29,8 +29,7 @@ export class STLRenderer extends ModelRenderer {
     animate() {
         this.id = requestAnimationFrame( this.animate.bind(this) )
 
-        this.mesh.rotation.x += 0.01
-        this.mesh.rotation.y += 0.02
+        this.mesh.rotation.z += 0.01
 
         this.renderer.render( this.scene, this.camera )
     }
