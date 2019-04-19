0212e7cac57a2174d851c85ff1dec613b62db1d5
tgru <21686590+tgru@users.noreply.github.com>
2019-04-19 23:29:10 +0200

    Fix a bug where resize(...) had no effect
    resize() had no effect because the updateProjectionMatrix() was never
    called.

--- js/STLRenderer.js
+++ js/STLRenderer.js
@@ -67,5 +67,6 @@ export class STLRenderer extends ModelRenderer {
     resize(width, height) {
         this.renderer.setSize(width, height)
         this.camera.aspect = width / height
+        this.camera.updateProjectionMatrix()
     }
 }
