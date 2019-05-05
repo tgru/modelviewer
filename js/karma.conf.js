0a55ea4522d11c716d1bd0499eb35fd31851bf4e
tgru <21686590+tgru@users.noreply.github.com>
2019-05-05 02:26:06 +0200

    Beautify config files

--- js/karma.conf.js
+++ js/karma.conf.js
@@ -1,16 +1,21 @@
 const path = require('path')
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     frameworks: ['jasmine'],
     browsers: ['ChromeHeadless'],
-    files: [
-      { pattern: 'spec/*Spec.js', watched: false },
-      { pattern: 'spec/**/*Spec.js', watched: false }
+    files: [{
+        pattern: 'spec/*Spec.js',
+        watched: false
+      },
+      {
+        pattern: 'spec/**/*Spec.js',
+        watched: false
+      }
     ],
     preprocessors: {
-      'spec/*Spec.js': [ 'webpack' ],
-      'spec/**/*Spec.js': [ 'webpack' ]
+      'spec/*Spec.js': ['webpack'],
+      'spec/**/*Spec.js': ['webpack']
     },
     webpack: require('./webpack.config.js'),
     webpackMiddleware: {
@@ -19,22 +24,23 @@ module.exports = function(config) {
     reporters: ['coverage'],
     coverageReporter: {
       reporters: [{
-        type: 'text-summary'
-      },
-      {
-        type: function () {
-          var shieldBadgeReporter = require('istanbul-reporter-shield-badge')
-          var istanbul = require('istanbul')
-          istanbul.Report.register(shieldBadgeReporter)
-          return 'shield-badge'
-        }(),
-        subdir: '.',
-        coverageType: 'statements',
-        range: [75, 90],
-        subject: 'Coverage',
-        readmeFilename: 'README.md',
-        readmeDir: path.resolve(__dirname, '..')
-      }
-    ]}
+          type: 'text-summary'
+        },
+        {
+          type: function () {
+            var shieldBadgeReporter = require('istanbul-reporter-shield-badge')
+            var istanbul = require('istanbul')
+            istanbul.Report.register(shieldBadgeReporter)
+            return 'shield-badge'
+          }(),
+          subdir: '.',
+          coverageType: 'statements',
+          range: [75, 90],
+          subject: 'Coverage',
+          readmeFilename: 'README.md',
+          readmeDir: path.resolve(__dirname, '..')
+        }
+      ]
+    }
   });
 };
