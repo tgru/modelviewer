678a3cd5194065e8464604ef3e4340d9a1ff481e
tgru <21686590+tgru@users.noreply.github.com>
2019-05-06 12:20:59 +0200

    Fix a bug which caused tests to fail/pass because of uninitialized variables

--- js/tests/STLRendererTest.js
+++ js/tests/STLRendererTest.js
@@ -4,31 +4,31 @@ describe("STLRenderer", function () {
     describe("constructor(width, height)", function () {
         it("Should build without error", function () {
             expect(function () {
-                renderer = new STLRenderer(800, 600)
+                let renderer = new STLRenderer(800, 600)
             }).not.toThrowError()
         })
 
         it("Should not accept a negative width", function () {
             expect(function () {
-                renderer = new STLRenderer(-800, 600)
+                let renderer = new STLRenderer(-800, 600)
             }).toThrowError()
         })
 
         it("Should not accept a negative height", function() {
             expect(function () {
-                renderer = new STLRenderer(800, -600)
+                let renderer = new STLRenderer(800, -600)
             }).toThrowError()
         })
 
         it("Should not accept zero width", function() {
             expect(function () {
-                renderer = new STLRenderer(0, 600)
+                let renderer = new STLRenderer(0, 600)
             }).toThrowError()
         })
 
         it("Should not accept zero height", function() {
             expect(function () {
-                renderer = new STLRenderer(800, 0)
+                let renderer = new STLRenderer(800, 0)
             }).toThrowError()
         })
     })
